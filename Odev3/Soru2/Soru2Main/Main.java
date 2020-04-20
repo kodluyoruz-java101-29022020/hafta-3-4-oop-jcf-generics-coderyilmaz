4aeb8f74c32867cc23636290ab117726cfc7f738
coderyilmaz <coderyilmaz@gmail.com>
2020-04-20 22:35:54 +0300

    Update

---
+++ Odev3/Soru2/Soru2Main/Main.java
@@ -0,0 +1,16 @@
+package Soru2Main;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		MyList integers = new MyList();
+		
+		integers.add(3);
+		integers.add(4);
+		
+		//System.out.println(integers.toString());
+		System.out.println(integers.length());
+	}
+
+}
